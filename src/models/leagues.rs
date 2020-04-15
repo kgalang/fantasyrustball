c378593b96ba62083cde0713b136020cd763ea43
kgalang <kgalang5@gmail.com>
2020-04-14 21:59:21 -0400

    Fix League struct and be able to query it. Add basic test.

--- src/models/leagues.rs
+++ src/models/leagues.rs
@@ -1,29 +1,43 @@
+
 use chrono::{NaiveDateTime, Utc};
 use uuid::Uuid;
+use crate::handlers::leagues::{LeagueResponse, LeaguesResponse};
+use crate::database::PoolType;
+use crate::errors::ApiError;
+use crate::schema::{leagues, league_rulesets};
+use diesel::prelude::*;
 
-#[derive(
-  Debug, PartialEq,
-  Serialize, Deserialize,
-  Queryable, Identifiable, Insertable,
-)]
-#[table_name = "leagues"]
+
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Queryable, Identifiable)]
 pub struct League {
   pub id: Uuid,
   pub name: String,
   pub start: NaiveDateTime,
-  pub rounds: u8,
-  pub current_round: u8,
+  pub rounds: i32,
+  pub current_round: Option<i32>,
 }
 
-#[derive(
-  Debug,
-  Serialize, Deserialize,
-  Queryable, Identifiable, Insertable,
-)]
-#[table_name = "league_rulesets"]
-pub struct Ruleset {
-  pub id: Uuid,
-  pub league_id: Uuid,
-  pub points_per_mile: i32,
+pub fn get_all(pool: &PoolType) -> Result<Vec<(League)>, ApiError> {
+  let conn = pool.get()?;
+  let all_leagues = leagues::table.load(&conn)?;
+
+  Ok(all_leagues)
 }
 
+
+#[cfg(test)]
+pub mod tests {
+  use super::*;
+  use crate::tests::helpers::tests::get_pool;
+
+  pub fn get_all_leagues() -> Result<Vec<(League)>, ApiError> {
+    let pool = get_pool();
+    get_all(&pool)
+  }
+
+  #[test]
+  fn it_gets_all_leagues() {
+    let leagues = get_all_leagues();
+    assert!(leagues.is_ok());
+  }
+}
