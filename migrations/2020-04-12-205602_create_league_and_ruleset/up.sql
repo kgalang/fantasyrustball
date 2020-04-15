9c5bf6a1957a673547d098bb78b0923cebf369b5
kgalang <kgalang5@gmail.com>
2020-04-14 21:58:16 -0400

    Update migration for ruleset to have default and add dummy data for dev.

--- migrations/2020-04-12-205602_create_league_and_ruleset/up.sql
+++ migrations/2020-04-12-205602_create_league_and_ruleset/up.sql
@@ -7,7 +7,13 @@ CREATE TABLE leagues (
 );
 
 CREATE TABLE league_rulesets (
-    id UUID NOT NULL PRIMARY KEY,
+    id UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
     points_per_mile INT NOT NULL,
     league_id UUID NOT NULL REFERENCES leagues (id) ON DELETE CASCADE
 );
+
+INSERT INTO leagues (id, name, start, rounds, current_round) VALUES 
+  ('00000000-0000-0000-0000-000000000000', 'Test Tournament', NOW(), 10, 0);
+
+INSERT INTO league_rulesets (league_id, points_per_mile) VALUES 
+  ('00000000-0000-0000-0000-000000000000', 100);
