fde1e7ef943e1123150143a8bb5c1174bead4040
kgalang <kgalang5@gmail.com>
2020-04-15 06:23:07 -0400

    Create ruleset struct for league_details table. Query join on leagues and league_details data.

--- src/models/leagues.rs
+++ src/models/leagues.rs
@@ -3,7 +3,7 @@ use chrono::NaiveDateTime;
 use uuid::Uuid;
 use crate::database::PoolType;
 use crate::errors::ApiError;
-use crate::schema::leagues;
+use crate::schema::{leagues, league_rulesets};
 use diesel::prelude::*;
 
 
@@ -16,9 +16,51 @@ pub struct League {
   pub current_round: Option<i32>,
 }
 
-pub fn get_all(pool: &PoolType) -> Result<Vec<League>, ApiError> {
+#[derive(Debug, Clone, Serialize, Deserialize, Queryable, Identifiable, Associations)]
+#[table_name = "league_rulesets"]
+pub struct Ruleset {
+  pub id: Uuid,
+  pub points_per_mile: i32,
+  pub league_id: Uuid,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Queryable)]
+pub struct LeagueDetails {
+  pub id: Uuid,
+  pub name: String,
+  pub start: NaiveDateTime,
+  pub rounds: i32,
+  pub current_round: Option<i32>,
+  pub points_per_mile: i32,
+}
+
+type LeagueDetailsColumns = (
+  leagues::id,
+  leagues::name,
+  leagues::start,
+  leagues::rounds,
+  leagues::current_round,
+  league_rulesets::points_per_mile,
+);
+
+pub const LEAGUE_DETAILS_COLUMNS: LeagueDetailsColumns = (
+  leagues::id,
+  leagues::name,
+  leagues::start,
+  leagues::rounds,
+  leagues::current_round,
+  league_rulesets::points_per_mile,
+);
+
+pub fn get_all_details(pool: &PoolType) -> Result<Vec<LeagueDetails>, ApiError> {
   let conn = pool.get()?;
-  let all_leagues = leagues::table.load(&conn)?;
+  let query = leagues::table.inner_join(league_rulesets::table)
+    .select(LEAGUE_DETAILS_COLUMNS);
+
+  let debug = diesel::debug_query::<diesel::pg::Pg, _>(&query).to_string();
+  println!("debug statement: {:?}\n", debug);
+  let all_leagues = query.load(&conn)?;
+  println!("{:?}\n", all_leagues);
 
   Ok(all_leagues)
 }
@@ -29,14 +71,14 @@ pub mod tests {
   use super::*;
   use crate::tests::helpers::tests::get_pool;
 
-  pub fn get_all_leagues() -> Result<Vec<League>, ApiError> {
+  pub fn get_all_leagues_with_details() -> Result<Vec<LeagueDetails>, ApiError> {
     let pool = get_pool();
-    get_all(&pool)
+    get_all_details(&pool)
   }
 
   #[test]
-  fn it_gets_all_leagues() {
-    let leagues = get_all_leagues();
+  fn it_gets_all_leagues_with_details() {
+    let leagues = get_all_leagues_with_details();
     assert!(leagues.is_ok());
   }
 }
