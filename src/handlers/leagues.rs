2c34ca7d670bd48c647b7250fb37c613fc0a8ff0
kgalang <kgalang5@gmail.com>
2020-04-15 22:16:31 -0400

    Add tests for getting all leagues in handler.

--- src/handlers/leagues.rs
+++ src/handlers/leagues.rs
@@ -32,3 +32,21 @@ impl From<Vec<LeagueDetails>> for LeaguesResponse {
         LeaguesResponse(leagues.into_par_iter().map(|league| league).collect())
     }
 }
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+    use crate::tests::helpers::tests::{get_data_pool, get_pool};
+
+    pub fn get_all_leagues() -> LeaguesResponse {
+        let pool = get_pool();
+        get_all_details(&pool).unwrap()
+    }
+
+    #[actix_rt::test]
+    async fn it_gets_all_tournaments() {
+        let response = get_leagues(get_data_pool()).await;
+        assert!(response.is_ok());
+        assert_eq!(response.unwrap().into_inner().0[0], get_all_leagues().0[0]);
+    }
+}
