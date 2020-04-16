8bac3a817de6af73c0f4901566dad2c4b755a61c
kgalang <kgalang5@gmail.com>
2020-04-15 22:06:05 -0400

    Change to 4 space indents to match the rest of the files.

--- src/handlers/leagues.rs
+++ src/handlers/leagues.rs
@@ -11,24 +11,24 @@ use chrono::{NaiveDateTime};
 
 #[derive(Debug, Clone, Deserialize, Serialize, PartialEq, Queryable)]
 pub struct LeagueDetails {
-  pub id: Uuid,
-  pub name: String,
-  pub start: NaiveDateTime,
-  pub rounds: i32,
-  pub current_round: Option<i32>,
-  pub points_per_mile: i32,
+    pub id: Uuid,
+    pub name: String,
+    pub start: NaiveDateTime,
+    pub rounds: i32,
+    pub current_round: Option<i32>,
+    pub points_per_mile: i32,
 }
 
 #[derive(Debug, Clone, Deserialize, Serialize, PartialEq)]
 pub struct LeaguesResponse(pub Vec<LeagueDetails>);
 
 pub async fn get_leagues(pool: Data<PoolType>) -> Result<Json<LeaguesResponse>, ApiError> {
-  let leagues: LeaguesResponse = block(move || get_all_details(&pool)).await?;
-  respond_json(leagues)
+    let leagues: LeaguesResponse = block(move || get_all_details(&pool)).await?;
+    respond_json(leagues)
 }
 
 impl From<Vec<LeagueDetails>> for LeaguesResponse {
-  fn from(leagues: Vec<LeagueDetails>) -> Self {
-      LeaguesResponse(leagues.into_par_iter().map(|league| league).collect())
-  }
+    fn from(leagues: Vec<LeagueDetails>) -> Self {
+        LeaguesResponse(leagues.into_par_iter().map(|league| league).collect())
+    }
 }
--- src/models/leagues.rs
+++ src/models/leagues.rs
@@ -10,66 +10,66 @@ use diesel::prelude::*;
 
 #[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Queryable, Identifiable)]
 pub struct League {
-  pub id: Uuid,
-  pub name: String,
-  pub start: NaiveDateTime,
-  pub rounds: i32,
-  pub current_round: Option<i32>,
+    pub id: Uuid,
+    pub name: String,
+    pub start: NaiveDateTime,
+    pub rounds: i32,
+    pub current_round: Option<i32>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, Queryable, Identifiable, Associations)]
 #[table_name = "league_rulesets"]
 pub struct Ruleset {
-  pub id: Uuid,
-  pub points_per_mile: i32,
-  pub league_id: Uuid,
+    pub id: Uuid,
+    pub points_per_mile: i32,
+    pub league_id: Uuid,
 }
 
 type LeagueDetailsColumns = (
-  leagues::id,
-  leagues::name,
-  leagues::start,
-  leagues::rounds,
-  leagues::current_round,
-  league_rulesets::points_per_mile,
+    leagues::id,
+    leagues::name,
+    leagues::start,
+    leagues::rounds,
+    leagues::current_round,
+    league_rulesets::points_per_mile,
 );
 
 pub const LEAGUE_DETAILS_COLUMNS: LeagueDetailsColumns = (
-  leagues::id,
-  leagues::name,
-  leagues::start,
-  leagues::rounds,
-  leagues::current_round,
-  league_rulesets::points_per_mile,
+    leagues::id,
+    leagues::name,
+    leagues::start,
+    leagues::rounds,
+    leagues::current_round,
+    league_rulesets::points_per_mile,
 );
 
 pub fn get_all_details(pool: &PoolType) -> Result<LeaguesResponse, ApiError> {
-  let conn = pool.get()?;
-  let query = leagues::table.inner_join(league_rulesets::table)
-    .select(LEAGUE_DETAILS_COLUMNS);
+    let conn = pool.get()?;
+    let query = leagues::table.inner_join(league_rulesets::table)
+        .select(LEAGUE_DETAILS_COLUMNS);
 
-  let debug = diesel::debug_query::<diesel::pg::Pg, _>(&query).to_string();
-  println!("debug statement: {:?}\n", debug);
-  let all_leagues = query.load(&conn)?;
-  println!("{:?}\n", all_leagues);
+    let debug = diesel::debug_query::<diesel::pg::Pg, _>(&query).to_string();
+    println!("debug statement: {:?}\n", debug);
+    let all_leagues = query.load(&conn)?;
+    println!("{:?}\n", all_leagues);
 
-  Ok(all_leagues.into())
+    Ok(all_leagues.into())
 }
 
 
 #[cfg(test)]
 pub mod tests {
-  use super::*;
-  use crate::tests::helpers::tests::get_pool;
+    use super::*;
+    use crate::tests::helpers::tests::get_pool;
 
-  pub fn get_all_leagues_with_details() -> Result<LeaguesResponse, ApiError> {
-    let pool = get_pool();
-    get_all_details(&pool)
-  }
+    pub fn get_all_leagues_with_details() -> Result<LeaguesResponse, ApiError> {
+        let pool = get_pool();
+        get_all_details(&pool)
+    }
 
-  #[test]
-  fn it_gets_all_leagues_with_details() {
-    let leagues = get_all_leagues_with_details();
-    assert!(leagues.is_ok());
-  }
+    #[test]
+    fn it_gets_all_leagues_with_details() {
+        let leagues = get_all_leagues_with_details();
+        assert!(leagues.is_ok());
+    }
 }
